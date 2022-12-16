16d0973e2988c122514ddbdb724e09c70a6ed297
csubirana <celian.subirana.pro@gmail.com>
2022-12-16 09:30:06 +0100

    MP23T3-9 <Création du squelette de la classe Partie>

---
+++ Partie.h
@@ -0,0 +1,16 @@
+#ifndef PARTIE_H
+    #define PARTIE_H
+#endif // !PARTIE_H #define PARTIE_H
+#include <iostream>
+
+class Partie
+{
+    private:
+        int nombreDeJoueurs;
+        const int objectifNombre = 343;
+        std::string difficulte;
+        int numeroDeTour;
+    public:
+        Partie();
+        Partie(int nombreDeJoueurs, std::string difficulte, int numeroDeTour);
+};
