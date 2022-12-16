444fc33fb07b724bcb9aa2987478e5f6a1d7557b
csubirana <celian.subirana.pro@gmail.com>
2022-12-16 10:27:45 +0100

    Formatage

--- Partie.h
+++ Partie.h
@@ -5,12 +5,13 @@
 
 class Partie
 {
-    private:
-        int nombreDeJoueurs;
-        const int objectifNombre = 343;
-        std::string difficulte;
-        int numeroDeTour;
-    public:
-        Partie();
-        Partie(int nombreDeJoueurs, std::string difficulte, int numeroDeTour);
+  private:
+    int nombreDeJoueurs;
+    const int objectifNombre = 343;
+    std::string difficulte;
+    int numeroDeTour;
+
+  public:
+    Partie();
+    Partie(int nombreDeJoueurs, std::string difficulte, int numeroDeTour);
 };
