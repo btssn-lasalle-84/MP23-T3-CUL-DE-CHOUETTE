4ba1e28bd49b9aa98007d595620607388fb1c4f3
m-salaun <matthieusalaun30@gmail.com>
2022-12-21 12:16:24 +0100

    MP23T3-10-creation-du-squelette-de-la-classe-joueur, const NOMBRE_DE_DES / NOMBRE_DE_POINTS, #ifndef

--- Joueur.h
+++ Joueur.h
@@ -1,11 +1,17 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
 #include <iostream>
 #include <vector>
+
+#define NOMBRE_DE_DES 3
+#define NOMBRE_DE_POINTS 343
 
 class De;
 class Joueur
 {
   private:
-    std::vector<De*> de[3];
+    std::vector<De*> de[NOMBRE_DE_DES];
     unsigned int     compteurDePoint;
     std::string      nomDuJoueur;
 
