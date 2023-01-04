8b849aabcd0c950361992aea4d96a40f422dd0e3
csubirana <celian.subirana.pro@gmail.com>
2023-01-04 11:53:20 +0100

    Correction de la classe Partie

--- Partie.cpp
+++ Partie.cpp
@@ -1,9 +1,12 @@
 #include "Partie.h"
 
-Partie::Partie()
+Partie::Partie() : objectifNombre(OBJECTIF_PAR_DEFAUT), difficulte(DIFFICULTE_PAR_DEFAUT), nombreDeJoueurs(NB_JOUEURS_PAR_DEFAUT), numeroDeTour(0)
 {
 }
 
-Partie::Partie(int nombreDeJoueurs) : nombreDeJoueurs(nombreDeJoueurs)
+Partie::Partie(int nombreDeJoueurs, unsigned int difficulte, int objectifNombre) : nombreDeJoueurs(nombreDeJoueurs),
+                                                                                   objectifNombre(objectifNombre),
+                                                                                   difficulte(difficulte),
+                                                                                   numeroDeTour(0)
 {
 }
--- Partie.h
+++ Partie.h
@@ -1,14 +1,17 @@
-#include <iostream>
+#include <string>
 
+#define OBJECTIF_PAR_DEFAUT 343
+#define DIFFICULTE_PAR_DEFAUT 0
+#define NB_JOUEURS_PAR_DEFAUT 2
 class Partie
 {
   private:
     int nombreDeJoueurs;
-    const int objectifNombre = 343;
-    std::string difficulte;
+    int objectifNombre;
+    unsigned int difficulte;
     int numeroDeTour;
 
   public:
     Partie();
-    Partie(int nombreDeJoueurs);
+    Partie(int nombreDeJoueurs, unsigned int difficulte, int objectifNombre);
 };
