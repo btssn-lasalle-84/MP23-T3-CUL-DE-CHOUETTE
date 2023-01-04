06d8e42084da3433b2181a99afc5d3e515c33927
csubirana <celian.subirana.pro@gmail.com>
2023-01-04 11:31:04 +0100

    MP23T3-11 création du squelette de la classe De

---
+++ De.cpp
@@ -0,0 +1,9 @@
+#include "De.h"
+
+De::De()
+{
+}
+
+De::De(int valeur, unsigned int nombreDeFace, int valeurTotale) : valeur(valeur), nombreDeFace(nombreDeFace), valeurTotale(valeurTotale)
+{
+}
---
+++ De.h
@@ -0,0 +1,16 @@
+#ifndef DE_H
+  #define DE_H
+#include <iostream>
+
+class De
+{
+  private:
+    int valeur;
+    unsigned int nombreDeFace;
+    int valeurTotale;
+
+  public:
+    De();
+    De(int valeur, unsigned int nombreDeFace, int valeurTotale);
+};
+#endif // !DES_H
