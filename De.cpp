3b34dfd20f28dce738c304acb49cd63f64b7a21b
csubirana <celian.subirana.pro@gmail.com>
2023-01-05 11:42:23 +0100

    Ajouts des méthodes de la Classe De

--- De.cpp
+++ De.cpp
@@ -1,6 +1,20 @@
 #include "De.h"
+#include <random>
 
 De::De(unsigned int nombreDeFaces /*= NOMBRE_DE_FACES*/) :
     valeur(INDEFINI), nombreDeFaces(nombreDeFaces)
 {
+}
+
+int De::getValeurDe()
+{
+    std::cout << this->valeur << std::endl;
+    return this->valeur;
+}
+
+void De::lancerDe()
+{
+    std::random_device                 nombre;
+    std::uniform_int_distribution<int> generer(NOMBRE_MIN_DE, NOMBRE_MAX_DE);
+    this->valeur = generer(nombre);
 }
--- De.h
+++ De.h
@@ -1,9 +1,14 @@
 #ifndef DE_H
 #define DE_H
 
+#include <string>
+#include <vector>
+#include <iostream>
+
 #define NOMBRE_DE_FACES 6
 #define INDEFINI        0
-
+#define NOMBRE_MIN_DE   1
+#define NOMBRE_MAX_DE   6
 class De
 {
   private:
@@ -12,6 +17,10 @@ class De
 
   public:
     De(unsigned int nombreDeFaces = NOMBRE_DE_FACES);
+
+    // méthodes
+    int  getValeurDe();
+    void lancerDe();
 };
 
 #endif // !DES_H
