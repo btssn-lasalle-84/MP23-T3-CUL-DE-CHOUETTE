b4dba0967f6de2b4abaa700fe89bbb7fed1277b2
csubirana <celian.subirana.pro@gmail.com>
2023-01-05 16:01:08 +0100

    completion de la méthode afficherRegle() + initialisation des autres méthodes d'affichage

--- Ihm.h
+++ Ihm.h
@@ -1,12 +1,20 @@
 #ifndef IHM_H
 #define IHM_H
 
+#include <iostream>
+#include <iomanip>
 class Ihm
 {
   private:
   public:
     Ihm();
     ~Ihm();
+
+    void afficherRegle() const;
+    void afficherDebutJeu() const;
+    void afficherJoueur() const;
+    void afficherLancer() const;
+    void afficherScoreDuJoueur() const;
 };
 
 #endif
